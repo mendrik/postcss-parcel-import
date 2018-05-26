44b7ffe10ab08d82691d50538bac723ef9fe0419
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-26 17:02:22 +0300

    prevent recursive imports

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ module.exports = postcss.plugin('postcss-parcel-import', function (opts) {
         root.walkAtRules('parcel-import', function(rule) {
             var relativeFile = rule.params.substr(1, rule.params.length - 2);
             var file = path.join(path.dirname(rule.source.input.file), relativeFile);
-            var content = fs.readFileSync(file, 'utf-8')
+            var content = fs.readFileSync(file, 'utf-8');
+            if (content.indexOf('@parcel-import') !== -1) {
+                throw Error('Recursive @parcel-import detected in ' + file);
+            }
             rule.replaceWith(content);
         });
     };
